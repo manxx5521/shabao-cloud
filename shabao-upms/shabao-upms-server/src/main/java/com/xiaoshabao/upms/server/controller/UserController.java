adea270d7fceca526876209e102411b78b13aeb3
manxx5521 <manxx5521@163.com>
2019-01-08 16:44:45 +0800

    1

--- shabao-upms/shabao-upms-server/src/main/java/com/xiaoshabao/upms/server/controller/UserController.java
+++ shabao-upms/shabao-upms-server/src/main/java/com/xiaoshabao/upms/server/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.xiaoshabao.upms.server.controller;
 
-import java.util.concurrent.TimeUnit;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
